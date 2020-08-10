7087ee8559a33c8386e8df447d362783749d8bc9
Ze Dang <4649tz@gmail.com>
2020-08-10 15:30:29 +0800

    [zedang] feat: finish ex-1

--- 1-string/templateString/index.js
+++ 1-string/templateString/index.js
@@ -7,4 +7,6 @@ function getScore(id) {
 
 export default function getScoreOutput(person) {
   // TODO 8: 在这里写实现代码
+  const { firstName, lastName, id } = person;
+  return `你好，${lastName}${firstName}！你的考试成绩为${getScore(id)}分`;
 }
